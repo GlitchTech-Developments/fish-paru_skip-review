ce52cd241749ed9802e45720656decf6f2ffce11
Jesse Koldewijn <60965908+JesseKoldewijn@users.noreply.github.com>
2022-11-20 22:03:33 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "......................................."
+echo "...Installing fish-paru lazy-wrapper..."
+echo "......................................."
+echo "\n"
+echo "...Starting copy process..."
+cp ./paru.nvm ~/.config/fish/functions/paru.nvm
+echo "...Done..."
